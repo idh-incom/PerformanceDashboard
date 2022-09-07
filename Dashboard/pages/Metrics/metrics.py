6f31b6e3850b2c56dda86eff6dc3fe96d7e59e47
idh-incom <73580258+idh-incom@users.noreply.github.com>
2022-09-07 06:01:34 +0200

    metrics update

--- Dashboard/pages/Metrics/metrics.py
+++ Dashboard/pages/Metrics/metrics.py
@@ -8,6 +8,8 @@ Created on Wed Feb 23 17:33:48 2022
 import pandas.io.sql as psql
 import pandas as pd
 import streamlit as st
+import datetime as dt
+import numpy as np
 from streamlit_autorefresh import st_autorefresh
 
 from sqlalchemy import create_engine
@@ -18,7 +20,7 @@ import warnings
 import matplotlib.pyplot as plt
 plt.style.use("ggplot")
 
-APP_AUTHOR = 'IDH+TLU'
+APP_AUTHOR = 'IDH'
 APP_NAME = 'daily_value_traded'
 bi_sql_engine: Engine = cast(Engine, create_engine(
     URL.create(
@@ -38,7 +40,12 @@ bi_sql_engine: Engine = cast(Engine, create_engine(
     max_overflow=5,
 ))
 
-def fetch_vals():
+daymap = {0: "Mon", 1: "Tue", 2: "Wed",
+          3: "Thu", 4: "Fri", 5: "Sat", 6: "Sun"}
+indmap = {True: "Weekday", False: "Weekend"}
+
+
+def fetch_traybot_volume_vals():
     query= f"""
             Select concat_ws('-', f.YearCet, f.MonthCet, f.DayCet) as [Date], f.Execution, sum(f.[Value]) as NominalValue, sum(f.[Volume]) as Volume from
             (SELECT
@@ -51,7 +58,6 @@ def fetch_vals():
             	  ,case when traderID in ('17688', '19118') then 'Traybot' else 'Manual' end as [Execution]
               FROM [pub].[GasTransactions]
               where bookId in (3001, 3002, 3015, 3017, 3027,3028,3029)
-              and TradeTimestampUtc > '2022-08-25'
               and TradeType = 1
               and traderID in ('17485', --RRH
                            '17688', --TraYBOT,
@@ -65,7 +71,8 @@ def fetch_vals():
                            '25215', --: "IDH",
                            '26457', --: "JPO",
                            '27338', --: "GBI"
-                           '28081'--: "SWO"
+                           '28081',--: "SWO"
+                           '28638' --; TAA
                            )
             	and Currency != 'GBP') f  -- do not want to include NBP here for now. Too much effort
             group by YearCet, MonthCet, DayCet, Execution
@@ -74,130 +81,131 @@ def fetch_vals():
     values = psql.read_sql_query(query,bi_sql_engine)
     return values
 
-if __name__ == '__main__':
-    df = fetch_vals()
-    df["Date"] = df["Date"].apply(pd.to_datetime)
-    # df = df.set_index("Date")
-    df2 = df.pivot(index='Date', columns='Execution', values=['NominalValue', "Volume"]).fillna(0)
-    df2["value_percentage_manual"] = df2.iloc[:,0]/(df2.iloc[:,:2].sum(axis=1))
-    df2["value_percentage_traybot"] = df2.iloc[:,1]/(df2.iloc[:,:2].sum(axis=1))
-    df2["volume_percentage_manual"] = df2.iloc[:,2]/(df2.iloc[:,2:4].sum(axis=1))
-    df2["volume_percentage_traybot"] = df2.iloc[:,3]/(df2.iloc[:,2:4].sum(axis=1))
-    moving_avg = df2.rolling(7).mean()
-    moving_avg.columns = ["Manual value", "Traybot value", "Manual Volume",
-                          "Traybot Volume", "Man_pct_value", "TB_pct_value",
-                          "Man_pct_volume", "TB_pct_volume"]
-    [moving_avg.loc["2021":].iloc[:,x:x+2].plot() for x in [0,2,4,6]] 
-    [df2.loc["2021":].iloc[:,x:x+2].plot() for x in [0,2,4,6]]
-    df2.to_csv("daily_vals_wide.csv")
-    moving_avg.to_csv("week_moving_average.csv")
-
+def plot_traybot_usage():
+    fetch_traybot_usage_data = st.button("Fetch usage data")
+    now = pd.to_datetime(dt.datetime.utcnow()).tz_localize("UTC").tz_convert("Europe/Berlin")
+    if 'last_usage_refresh' not in st.session_state:
+        st.session_state.last_usage_refresh = pd.to_datetime('2022-01-01').tz_localize("UTC").tz_convert("Europe/Berlin")
+    conds = (fetch_traybot_usage_data, (np.array([x in st.session_state for x in ['usage_data', 'usage_ma', 'last_usage_refresh']]) == False).any(), (now - st.session_state.last_usage_refresh).days > 0)
+    if any(conds):
+        st.write("refreshing data, please wait...")
+        df = fetch_traybot_volume_vals()
+        df["Date"] = df["Date"].apply(pd.to_datetime)
+        df2 = df.pivot(index='Date', columns='Execution', values=['NominalValue', "Volume"]).fillna(0)
+        df2.index = df2.index.tz_localize("UTC").tz_convert("Europe/Berlin")
+        df2["value_percentage_manual"] = df2.iloc[:,0]/(df2.iloc[:,:2].sum(axis=1))
+        df2["value_percentage_traybot"] = df2.iloc[:,1]/(df2.iloc[:,:2].sum(axis=1))
+        df2["volume_percentage_manual"] = df2.iloc[:,2]/(df2.iloc[:,2:4].sum(axis=1))
+        df2["volume_percentage_traybot"] = df2.iloc[:,3]/(df2.iloc[:,2:4].sum(axis=1))
+        moving_avg = df2.rolling(7).mean()
+        moving_avg.columns = ["Manual value", "Traybot value", "Manual Volume",
+                              "Traybot Volume", "Man_pct_value", "TB_pct_value",
+                              "Man_pct_volume", "TB_pct_volume"]
+        df2.columns = ["Manual value", "Traybot value", "Manual Volume",
+                              "Traybot Volume", "Man_pct_value", "TB_pct_value",
+                              "Man_pct_volume", "TB_pct_volume"]
+        st.session_state.usage_data = df2.copy()
+        st.session_state.usage_ma = moving_avg.copy()
+        st.session_state.last_usage_refresh = pd.to_datetime(dt.datetime.utcnow()).tz_localize("UTC").tz_convert("Europe/Berlin")
+    st.write("last refresh", st.session_state.last_usage_refresh.strftime("%Y-%m-%d %H:%M:%S"))
+    fig, axs = plt.subplots(2,1, sharex=True)
+    [st.session_state.usage_ma.loc["2021":].iloc[:,x:x+2].plot(ax=axs[i]) for i,x in enumerate([2,6])]
+    fig.suptitle("7 day moving average of traybot usage (absolute/relative)")
+    l,r = st.beta_columns(2)
+    with l:
+        st.pyplot(fig)
+    with r:
+        st.dataframe(st.session_state.usage_data.iloc[:,[2,3,6,7]].tail(15), height=800)
 
 # a.to_csv("Daily_values_per_us_or_not.csv", index=False)
 # a = pd.read_csv("Daily_values_per_us_or_not.csv")
 
-sq = """
-select s.Date, s.isOwnData, sum(val) as [Value]
-from
-(SELECT a.*,
- 	  CONVERT(date, a.DateTimeCet) as [Date]
- 	  , (25 - datepart(hour, a.DateTimeCet)) % 24 + 1 as RoDHours
- 	  , case when InstId in (2,6,7) then Price*Volume*24
- 	  when InstId = 1 then Price*Volume*((25 - datepart(hour, a.DateTimeCet)) % 24 + 1)
- 	  when InstId = 4 then Price*Volume*48
- 	  else Price*Volume
- 	  end as [val]
- 	  from
- 	  (
- 			SELECT [TradeID]
-				,[RouteID]
- 			  ,[RouteName]
- 			  ,[Action]
- 			  ,[DateTime]
- 			  ,[Price]
- 			  ,[Volume]
- 			  ,[AggressorAction]
- 			  ,[AggressorBroker]
- 			  ,[AggressorBrokerID]
- 			  ,[InitiatorAction]
- 			  ,[InitiatorBroker]
- 			  ,[InitiatorBrokerID]
- 			  ,[LastUpdate]
- 			  ,[ForeignLastUpdate]
- 			  ,[ManualDeal]
- 			  ,[VoiceDeal]
- 			  ,[InitSleeve]
- 			  ,[AggSleeve]
- 			  ,[PNC]
- 			  ,[ClearingStatus]
- 			  ,[InitiatorOwnedSpread]
- 			  ,[AggressorOwnedSpread]
- 			  ,[UnderInvestigation]
- 			  ,[JTT]
- 			  ,[FromBrokenSpread]
- 			  ,[OtcGiveUp]
- 			  ,[ExecutionVenueID]
- 			  ,[ForeignContractID]
- 			  ,[IsMarketData]
- 			  ,[IsOwnData]
- 			  ,[InstID]
- 			  ,[InstName]
- 			  ,[FirstSequenceID]
- 			  ,[SeqSpan]
- 			  ,[FirstSequenceItemID]
- 			  ,[FirstSequenceItemName]
- 			  ,[TermFormatID]
- 			  ,[Unit]
- 			  ,[CurrencyIsoCode]
- 			  ,[ExecutionWorkflow]
- 			  ,[RowInsertedOrModifiedDateUTC]
- 			  ,[KafkaInsertedDateUTC]
- 			  , cast(DateTime at TIME ZONE 'UTC' AT TIME ZONE 'Romance Standard Time' as datetime) as DateTimeCet
-		  FROM [pub].[Trayport_Trades_Public]
-  where DateTime > '2022-03-25'
-  --and SeqSpan = 'Single'
-  and ((InstId in ('10000310', '10000312')) or (FirstSequenceItemID in (1, 2,4,6,7,30)))
-  --and FirstSequenceItemId = 2
-  ) a) s
-  group by isOwnData, [Date]
-"""
-a = psql.read_sql_query(sq, bi_sql_engine)
-b = a.pivot(index='Date', columns = 'isOwnData', values='Value').reset_index()
+def grab_trades_data(from_date):
+    sq = f"""
+    select s.Date, s.isOwnData, s.FirstSequenceItemName as [Venue], sum(val) as [Volume]
+    from
+    (SELECT a.*,
+     	  CONVERT(date, a.DateTimeCet) as [Date]
+     	  , (25 - datepart(hour, a.DateTimeCet)) % 24 + 1 as RoDHours
+     	  , case when InstName like '%PEG%' then Volume
+           when InstName like '%NBP%' then 29.3071*Volume
+           when FirstSequenceItemID in (2,6,7) then Volume*24
+     	  when FirstSequenceItemID = 1 then Volume*((25 - datepart(hour, a.DateTimeCet)) % 24 + 1)
+     	  when FirstSequenceItemID = 4 then Volume*48
+     	  else Volume
+     	  end as [val]
+     	  from
+     	  (
+     			SELECT [Action]
+     			  ,[DateTime]
+     			  ,[Price]
+     			  ,[Volume]
+     			  ,[IsMarketData]
+     			  ,[IsOwnData]
+     			  ,[InstID]
+     			  ,[InstName]
+     			  ,[FirstSequenceItemID]
+                  , case when InstId in ('10000310', '10000312') then 'Hourlies' else [FirstSequenceItemName] end as [FirstSequenceItemName]
+     			  ,[Unit]
+     			  , cast(DateTime at TIME ZONE 'UTC' AT TIME ZONE 'Romance Standard Time' as datetime) as DateTimeCet
+    		  FROM [pub].[Trayport_Trades_Public]
+      where DateTime > '{from_date}'
+      --and SeqSpan = 'Single'
+      and ((InstId in ('10000310', '10000312')) or (FirstSequenceItemID in (1, 2,4,6,7,30)))
+      --and FirstSequenceItemId = 2
+      ) a) s
+      group by isOwnData, [Date], FirstSequenceItemName
+    """
+    df = psql.read_sql_query(sq, bi_sql_engine)
+    return df
 
-b.columns = ["Date","Market_value", "Our_value"]
-b["percentage_InCo"] = b["Our_value"]/b[["Market_value","Our_value"]].sum(axis=1)
-b.Date = b.Date.apply(pd.to_datetime)
-plt.close('all')
-daymap = {0: "Mon", 1: "Tue", 2: "Wed",
-          3: "Thu", 4: "Fri", 5: "Sat", 6: "Sun"}
-indmap = {True: "Weekday", False: "Weekend"}
+def plot_market_share_data():
+    renew_trades_data = st.button("Fetch usage data")
+    now = pd.to_datetime(dt.datetime.utcnow()).tz_localize("UTC").tz_convert("Europe/Berlin")
+    if 'last_share_refresh' not in st.session_state:
+        st.session_state.last_share_refresh = pd.to_datetime('2021-01-01').tz_localize("UTC").tz_convert("Europe/Berlin")
+    conds = (renew_trades_data, (np.array([x in st.session_state for x in ['share_data', 'volume_data', 'last_share_refresh']]) == False).any(), (now - st.session_state.last_usage_refresh).total_seconds() > 300)
+    if any(conds):
+        if 'trades_data' not in st.session_state:
+            st.session_state.trades_data = grab_trades_data(st.session_state.last_share_refresh)
+        else:
+            st.session_state.trades_data = pd.concat([st.session_state.trades_data,grab_trades_data(st.session_state.last_share_refresh)])
+        
+        st.write(st.session_state.trades_data)
+        venues = np.array([x for x in st.session_state.trades_data.Venue.unique()])
+        products_to_include = np.array([st.checkbox(x) for x in venues])
+        a = st.session_state.trades_data.loc[st.session_state.trades_data.Venue.isin(products_to_include)]
+        b = a.pivot(index='Date', columns = 'isOwnData', values='Value').reset_index()
+        b.columns = ["Date","Market_value", "Our_value"]
+        b["percentage_InCo"] = b["Our_value"]/b[["Market_value","Our_value"]].sum(axis=1)
+        b.Date = b.Date.apply(pd.to_datetime)
+        
+        b["dayofweek"] = b.Date.dt.dayofweek
+        b["weekday"] = b["dayofweek"] < 5
+        b["weekday"] = b["weekday"].apply(lambda x: indmap[x])
+        b["dayofweek"] = b["dayofweek"].apply(lambda x: daymap[x])
 
-b["dayofweek"] = b.Date.dt.dayofweek
-b["weekday"] = b["dayofweek"] < 5
-b["weekday"] = b["weekday"].apply(lambda x: indmap[x])
-b["dayofweek"] = b["dayofweek"].apply(lambda x: daymap[x])
+        b = b.set_index("Date")
+        b = b.sort_index()
+        b.percentage_InCo.plot()
 
-b = b.set_index("Date")
-b = b.sort_index()
-b.percentage_InCo.plot()
-
-plt.title("Fraction of prompt market share In Commodities")
-plt.figure()
-c = b.groupby("dayofweek")["percentage_InCo"]
-c.plot(style='-*', legend=True)
-plt.title("Fraction of prompt market share In Commodities")
+        plt.title("Fraction of prompt market share In Commodities")
+        plt.figure()
+        c = b.groupby("dayofweek")["percentage_InCo"]
+        c.plot(style='-*', legend=True)
+        plt.title("Fraction of prompt market share In Commodities")
+        
+        
+        plt.figure()
+        d = b.groupby([pd.Grouper(freq='W-SUN'),
+                        "weekday"])["percentage_InCo"].mean().unstack()
+        d.index = d.index.date
+        d.plot(kind='bar', legend=True)
+        # plt.legend(["Weekday", "Weekend"])
+        plt.title("Fraction of prompt market share In Commodities")
 
 
-plt.figure()
-d = b.groupby([pd.Grouper(freq='W-SUN'),
-               "weekday"])["percentage_InCo"].mean().unstack()
-d.index = d.index.date
-d.plot(kind='bar', legend=True)
-# plt.legend(["Weekday", "Weekend"])
-plt.title("Fraction of prompt market share In Commodities")
-
-
-# traders = pd.read_clipboard()
+# # traders = pd.read_clipboard()
 # uni_traders = set(traders["day trader"].values).union(set(traders["night trader"].values))
 
 # for trader in uni_traders:
--- Dashboard/website_local.py
+++ Dashboard/website_local.py
@@ -17,38 +17,14 @@ except Exception as e:
     pass
 
 
-from UK_tool.pages.UK.UK_supply_demand_data import plot_UK_linepack
-page = st.sidebar.radio("Page selector", ["UK"], index=0)
-
-# st.session_state[""]
+from Dashboard.pages.Metrics.metrics import plot_traybot_usage, plot_market_share_data
+page = st.sidebar.radio("Page selector", ["Traybot_usage", "Market_share", "Trades_scatter"], index=0)
 
     
-
-if page == "UK":
-    plot_UK_linepack()
-    detail = st.sidebar.radio("Do we need more detail on a subarea?", ("No", "storages", "LNG", "imports", "plants", "supply_demand_forecast", "supply_demand_progression"))
-    choices = {"storages": "storages_hist", "LNG": "LNG_hist", "imports": "imports_hist", "plants": "plants_hist", "supply_demand_forecast": "fc_data_hist", "supply_demand_progression": "supdemdata_hist"}
-    l,m,r = st.beta_columns(3)
-    if detail != "No":
-        df = st.session_state[choices[detail]].copy()
-        cols = df.columns
-        subseries = np.array([st.sidebar.checkbox(x,True) for x in cols])
-        df2 = df[cols[subseries]]
-        with l:
-            fig4 = plt.figure()
-            ax4 = fig4.add_subplot(1,1,1)
-            df2.plot(ax=ax4)
-            st.write(fig4)
-        with r:
-            st.dataframe(df2)
-    with m:
-        fig5 = plt.figure()
-        ax5 = fig5.add_subplot(1,1,1)
-        
-        st.session_state["fpn_ccgt"].Value.plot(ax=ax5)
-        plt.ylabel("GW")
-        plt.title("CCGT FPN")
-        st.write(fig5)
+if page == "Traybot_usage":
+    plot_traybot_usage()
+elif page == 'Market_share':
+    plot_market_share_data()
 
 else:
     st.write("TO BE IMPLEMENTED")
